1f55ce944f1c7ef87c645ee8e3376f74944fc287
lars_marten <lars.marten@gmx.de>
2021-05-20 17:16:31 +0200

    use JWT secret from environment variable

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -38,7 +38,10 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
         return res.status(401).send({ message: 'No authorization headers.' });
     }
     
-
+    /*
+    * Header e.g.:
+    * Bearer dsfajfasjfajfaöjf;asfd.saf.
+    */
     const token_bearer = req.headers.authorization.split(' ');
     if(token_bearer.length != 2){
         return res.status(401).send({ message: 'Malformed token.' });
@@ -46,7 +49,7 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
     
     const token = token_bearer[1];
 
-    return jwt.verify(token, "hello", (err, decoded) => {
+    return jwt.verify(token, config.jwt.secret, (err, decoded) => {
       if (err) {
         return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
       }
