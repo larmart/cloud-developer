715cfd96a4cefcb125edddf3114a937df5354fc6
lars_marten <lars.marten@gmx.de>
2021-06-06 13:41:25 +0200

    Bugfix Resizing image => Error: Unsupported MIME type: -1

--- course-04/exercises/lesson-4/solution/src/lambda/s3/resizeImage.ts
+++ course-04/exercises/lesson-4/solution/src/lambda/s3/resizeImage.ts
@@ -36,7 +36,7 @@ async function processImage(record: S3EventRecord) {
 
   console.log('Resizing image')
   image.resize(150, Jimp.AUTO)
-  const convertedBuffer = await image.getBufferAsync(Jimp.AUTO.toString());
+  const convertedBuffer = await image.getBufferAsync(Jimp.MIME_JPEG);
 
   console.log(`Writing image back to S3 bucket: ${thumbnailBucketName}`)
   await s3
