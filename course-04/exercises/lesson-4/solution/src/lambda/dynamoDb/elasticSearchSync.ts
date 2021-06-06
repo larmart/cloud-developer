d52b5bbfebfab70ee3a7355a79a45d913d4dad5c
lars_marten <lars.marten@gmx.de>
2021-06-06 10:37:49 +0200

    Lesson4 EventProcessing 14. Enable DynamoDB Stream
    Integrate ElasticSearch

--- course-04/exercises/lesson-4/solution/src/lambda/dynamoDb/elasticSearchSync.ts
+++ course-04/exercises/lesson-4/solution/src/lambda/dynamoDb/elasticSearchSync.ts
@@ -1,19 +1,19 @@
 import { DynamoDBStreamEvent, DynamoDBStreamHandler } from 'aws-lambda'
 import 'source-map-support/register'
-//import * as elasticsearch from 'elasticsearch'
-//import * as httpAwsEs from 'http-aws-es'
+import * as elasticsearch from 'elasticsearch'
+import * as httpAwsEs from 'http-aws-es'
 
-/*const esHost = process.env.ES_ENDPOINT
+const esHost = process.env.ES_ENDPOINT
 
 const es = new elasticsearch.Client({
   hosts: [ esHost ],
   connectionClass: httpAwsEs
-})*/
+})
 
 export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
   console.log('Processing events batch from DynamoDB', JSON.stringify(event))
 
-  /*for (const record of event.Records) {
+  for (const record of event.Records) {
     console.log('Processing record', JSON.stringify(record))
     if (record.eventName !== 'INSERT') {
       continue
@@ -38,5 +38,5 @@ export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent)
       body
     })
 
-  }*/
+  }
 }
